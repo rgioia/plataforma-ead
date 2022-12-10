d1af1d3e3141f46d35f9eda3d64693dbb93a77c0
Rafael Gióia <rafaelbgioia@gmail.com>
2022-12-10 13:27:05 -0300

    feat: Integração de layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import './add_jquery'
 import * as bootstrap from "bootstrap"
+import "./editor"
+import "./form-validator.min"
+import "./jbox.all.min"
+import "./geticons"
+import "./metismenu.min"
+import "./owl.carousel.min"
+import "./simplebar.min"
+import "./sweetalert2.all.min"
+import "./custom"
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,10 @@ module PlataformaEad
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    I18n.load_path    += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.time_zone  = 'Brasilia'
+
+    config.i18n.available_locales = %i[en pt-BR]
+    config.i18n.default_locale = 'pt-BR'
   end
 end
