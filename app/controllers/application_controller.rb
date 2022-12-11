01a033ef343d9ea1b3b05ca19a5d834772b2b32c
Rafael Gióia <rafaelbgioia@gmail.com>
2022-12-11 15:54:47 -0300

    feat: removendo código não utilizado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,6 @@ class ApplicationController < ActionController::Base
 
   def search_courses
     @courses_header_search = Course.accessible_by(current_ability).ransack(params[:q])
-    @courses_header_search.sorts = 'name asc' if @courses_header_search.sorts.empty?
-    @courses = @courses_header_search.result.paginate(page: params[:page], per_page: 10)
   end
 
   protected
